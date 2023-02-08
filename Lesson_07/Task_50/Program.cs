7420ad816987f0835e6210a9244947880cceccb0
Vasiliy Gorilev <89159793029@ya.ru>
2023-02-08 12:34:04 +0300

    add Task 50

--- Lesson_07/Task_50/Program.cs
+++ Lesson_07/Task_50/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//___cod
+
+int[,] array = GenerateArray(4, 4, 0, 8);
+PrintArray2D(array);
+System.Console.Write("Число для поиска: ");
+int i = UserInput();
+
+int a = SerchNumb(array, i);
+if (a > 0) Console.WriteLine($"Число {i} -> присутсвует в массиве");
+else Console.WriteLine($"{i} -> такого числа нет в массиве");
+
+//___func
+
+void PrintArray2D(int[,] array) //вывод
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] GenerateArray(int m, int n, int min, int max) //генерация
+{
+    int[,] arrNum = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < arrNum.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrNum.GetLength(1); j++)
+        {
+            arrNum[i, j] = random.Next(min, max + 1);
+        }
+    }
+    return arrNum;
+}
+
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+
+    return number;
+}
+
+int SerchNumb(int[,] array, int numb)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == numb)
+                count++;
+        }
+    }
+    return count;
+}
