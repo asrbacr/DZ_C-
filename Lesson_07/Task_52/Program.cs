c2896c76caa27c5e4ee2eb1b05e93e37dcf2a490
Vasiliy Gorilev <89159793029@ya.ru>
2023-02-08 13:11:46 +0300

    add task 52, но работает не правильно

---
+++ Lesson_07/Task_52/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+//___cod
+
+int[,] array = GenerateArray(3, 4, 0, 8);
+PrintArray2D(array);
+
+double[]arr = AverColumnArray(array);
+System.Console.Write("Средние значения столбцов: ");
+PrintArray(arr);
+
+//___func
+
+void PrintArray2D(int[,] array) //вывод
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void PrintArray(double[] array) //вывод
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[,] GenerateArray(int m, int n, int min, int max) //генерация
+{
+    int[,] arrNum = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < arrNum.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrNum.GetLength(1); j++)
+        {
+            arrNum[i, j] = random.Next(min, max + 1);
+        }
+    }
+    return arrNum;
+}
+
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+
+    return number;
+}
+
+double[] AverColumnArray(int[,] array)
+{
+    double[] newArray = new double[array.GetLength(1)];
+    double SredNumbs = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            SredNumbs += array[i,j];
+        }
+        //SredNumbs / array.GetLength(1);
+        newArray[j] = SredNumbs / array.GetLength(1);
+    }
+    return newArray;
+}
