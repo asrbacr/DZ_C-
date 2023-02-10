ac6beb62e0d7ed4a6cf55a58c355f1923291516f
Vasiliy Gorulev <89159793029@ya.ru>
2023-02-10 19:13:33 +0300

    add sabl task 54

---
+++ Lesson_08/Task_54/Program.cs
@@ -0,0 +1,61 @@
+﻿//__code
+
+int[,] array = GenerateArray(3, 4, 1, 9);
+PrintArray2D(array);
+System.Console.WriteLine();
+SortArray(array);
+PrintArray2D(array);
+
+
+
+//__func
+
+void PrintArray2D(int[,] array) //вывод
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] GenerateArray(int m, int n, int min, int max) //генерация
+{
+    int[,] arrNum = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < arrNum.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrNum.GetLength(1); j++)
+        {
+            arrNum[i, j] = random.Next(min, max + 1);
+        }
+    }
+    return arrNum;
+}
+
+
+int[,] SortArray(int[,] array)
+{
+    int max = array[0, 0];
+    int[,] newArray = new int[array.GetLength(0), array.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (max < array[i, j])
+            {
+                max = array[i, j];
+                newArray[i, j] = max;
+            }
+            // else
+            //   newArray[i, j] = array[i, j];
+        }
+    }
+    return newArray;
+}
