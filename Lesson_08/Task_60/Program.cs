e2af4b32b9f87ab5a1927b7560298e6ac5d1bd2a
Vasiliy Gorilev <89159793029@ya.ru>
2023-02-14 13:12:49 +0300

    add sabl task 60

---
+++ Lesson_08/Task_60/Program.cs
@@ -0,0 +1,38 @@
+﻿int[,,] arr3D = new int[2, 2, 2];
+FillArray(arr3D);
+System.Console.WriteLine();
+PrintArray(arr3D);
+
+
+//___func
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void FillArray(int[,,] array)
+{
+
+    // int count = 20;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[k, i, j]; //+= count;
+                // count += 5;
+            }
+        }
+    }
+}
