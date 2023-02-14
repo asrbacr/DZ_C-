9c5bc6d54c80576071dac5b2f02903b3d5cbb1ba
Vasiliy Gorulev <89159793029@ya.ru>
2023-02-14 19:24:05 +0300

    add task 64

---
+++ Lesson_09/Task_64/Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.Write("Введите число до которого будем считать от 1-ого: ");
+int n = UserInput();
+PrintNumber(1, n);
+
+//____func 
+
+void PrintNumber(int M, int N)
+{
+    if (M > N)
+    {
+        PrintNumber(M - 1, N);
+        Console.Write($", {M}");
+    }
+    else if (M < N)
+    {
+        PrintNumber(M, N - 1);
+        Console.Write($", {N}");
+    }
+    else if (M == N)
+    {
+        Console.Write($"{M}");
+    }
+}
+
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+    return number;
+}
