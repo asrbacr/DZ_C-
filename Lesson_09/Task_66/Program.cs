dd3f90880c005fbe239702ef7ae667d62c2adfd3
Vasiliy Gorulev <89159793029@ya.ru>
2023-02-14 19:47:38 +0300

    add task 66

---
+++ Lesson_09/Task_66/Program.cs
@@ -0,0 +1,36 @@
+﻿System.Console.Write("Введите число от которого будем считать: ");
+int m = UserInput();
+System.Console.Write("Введите число до которого будем считать: ");
+int n = UserInput();
+System.Console.Write("Сумма ");
+FuncSummElements(m, n);
+
+//____func 
+void FuncSummElements(int M, int N)
+{
+    System.Console.Write(SummElements(M - 1, N));
+}
+
+int SummElements(int M, int N)
+{
+    int summ = M;
+    if (M == N)
+    {
+        return 0;
+    }
+    else
+    {
+        M++;
+        summ = M + SummElements(M, N);
+        return summ;
+    }
+
+}
+
+int UserInput()
+{
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+        Console.Write("Вы ввели не число. Повторите ввод: ");
+    return number;
+}
