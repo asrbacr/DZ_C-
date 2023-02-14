33b84b2164c57985884cdf64ad0ef9d04e2bfac7
Vasiliy Gorulev <89159793029@ya.ru>
2023-02-14 18:36:01 +0300

    Updata task 60

--- Lesson_08/Task_60/Program.cs
+++ Lesson_08/Task_60/Program.cs
@@ -23,15 +23,15 @@ void PrintArray(int[,,] array)
 void FillArray(int[,,] array)
 {
 
-    // int count = 20;
+    int count = 7;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                array[k, i, j]; //+= count;
-                // count += 5;
+                array[k, i, j] += count;
+                count += 3;
             }
         }
     }
